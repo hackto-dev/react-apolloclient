6c378331f6e3a66d938d6402edf4612699929857
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-06-10 19:25:28 +0530

    Set up React boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Components
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ninja Reading List</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
